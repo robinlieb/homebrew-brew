0c821ddccaf98ea4bb85f9563f39b07cd94d5932
Robin Lieb <robin.j.lieb@gmail.com>
2022-08-28 20:58:17 +0200

    feat: added validate-csv formula

---
+++ Formula/validate-csv.rb
@@ -0,0 +1,5 @@
+class ValidateCsv < Formula
+    desc "CLI tool to validate CSV by duplicates and sums of rows."
+    homepage "https://github.com/robinlieb/validate-csv"
+    license "Apache-2.0"
+  end
